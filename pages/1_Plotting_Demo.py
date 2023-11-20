81b6d21fc516ffb101b06c4a3ff80a385da9d776
GhiyasAsh-Shidiqie <ghiyasashidiqie42@gmail.com>
2023-11-20 16:00:03 +0700

    new file:   Homepage.py 	new file:   pages/1_Plotting_Demo.py 	new file:   pages/2_Mapping_Demo.py 	deleted:    streamlit_app.py

--- pages/1_Plotting_Demo.py
+++ pages/1_Plotting_Demo.py
@@ -2,32 +2,8 @@ import streamlit as st
 import pandas as pd
 from streamlit_gsheets import GSheetsConnection
 
-st.title("Google Sheets as a DataBase")
+st.title("View DataBase")
 
-# Function to create a sample Orders dataframe
-def create_orders_dataframe():
-    return pd.DataFrame({
-        'OrderID': [101, 102, 103, 104, 105],
-        'CustomerName': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
-        'ProductList': ['ProductA, ProductB', 'ProductC', 'ProductA, ProductC', 'ProductB, ProductD', 'ProductD'],
-        'TotalPrice': [200, 150, 250, 300, 100],
-        'OrderDate': ['2023-08-18', '2023-08-19', '2023-08-19', '2023-08-20', '2023-08-20']
-    })
-
-# Create the Orders dataframe
-orders = create_orders_dataframe()
-
-# Update the TotalPrice column in the orders dataframe to create updated_orders
-updated_orders = orders.copy()
-updated_orders['TotalPrice'] = updated_orders['TotalPrice'] * 100
-
-with st.expander("Data ⤵"):
-    st.write("Orders")
-    st.dataframe(orders)
-    st.write("Updated Orders")
-    st.dataframe(updated_orders)
-
-st.divider()
 st.write("CRUD Operations:")
 # Establishing a Google Sheets connection
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
