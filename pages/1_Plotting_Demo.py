ef414e4ca099e588a6a1747a69aa5793a4f2163a
GhiyasAsh-Shidiqie <ghiyasashidiqie42@gmail.com>
2023-11-21 07:35:15 +0700

    fix range

--- pages/1_Plotting_Demo.py
+++ pages/1_Plotting_Demo.py
@@ -45,7 +45,7 @@ input1 = st.text_input('Masukan Tahun Awal')
 input2 = st.text_input('Masukan Tahun Akhir')
 
 if st.button("Hasil Range Tahun"):
-    sql = 'SELECT * FROM Bigbooks WHERE date BETWEEN ?date AND ?date;'
+    sql = 'SELECT * FROM Bigbooks WHERE date BETWEEN date = :date AND date = :date;'
     range = conn.query(sql=sql, ttl=10, params={"date":"2013-03-25","date":"2013-03-26"})  # default ttl=3600 seconds / 60 min
     st.dataframe(range)
 
